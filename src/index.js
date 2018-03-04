f4a59cd83a2981afa477ff44ddc333e40b9c8da5
Aleh <32686077+Gonokokk@users.noreply.github.com>
2018-03-04 16:48:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+
+	var i, j;
+    for(i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+              if(matrix[i][j] === 0){
+                  matrix[i][j] = [];
+            }
+        }
+    }
+    return matrix;
+};
